ebac3b884d22aee27d69ef524d1fe3ad02098eb9
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-14 17:32:09 -0700

    Printing string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main(){
+/**
+ * main - entry point
+ *
+ * Return: return 0
+ */
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
