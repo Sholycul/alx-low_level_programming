933de1fd3bc43a45a93f6f09282207ea618cc0b9
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-16 15:23:18 -0700

    Swapping function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ *
+ * Return: the variable n.
+ */
+
+int _strlen(char *s)
+{
+	int n = strlen(s);
+
+	return (n);
+}
