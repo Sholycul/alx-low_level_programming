ba5fe3ee1f37a76e9708ab311423854a01b200a9
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-16 13:11:54 -0700

    Reset_to_98 project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates the value it points to to 98.
+ *
+ * @n: A pointer to an integer.
+ *
+ * Return: void
+ */
+
+int reset_to_98(int *n)
+{
+	*n = 98;
+
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Content of main.h goes here */
+
+#endif /* MAIN_H */
+
