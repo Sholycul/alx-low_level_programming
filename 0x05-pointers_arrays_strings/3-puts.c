83bbe9d34d90f3db18972e93875c19084a2386d8
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-16 15:44:29 -0700

    Printing with puts()

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - to print out
+ *
+ * @str: string variable
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - to print out
+ *
+ * @str: string variable
+ */
