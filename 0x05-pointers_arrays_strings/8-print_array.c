f2b0db6de58ce71f1505e65df3fd497d0c406434
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-17 10:20:18 -0700

    Concatenate array element

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_array - Prints elements of an integer array.
+ * @a: Pointer to the array of integers.
+ * @n: Number of elements to print.
+ *
+ * This function prints the elements of the integer array a
+ * up to n elements, separated by commas and followed by a space.
+ * If it's the last element, no comma and space are added.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		} else
+		{
+			printf("%d\n", a[i]);
+		}
+
+	}
+}
