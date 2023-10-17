5de1052bcad7f1714db47e2ba62f239dcd333b0a
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-17 04:33:56 -0700

    Printing after twos

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
-#include <string.h>
 
 /**
- * puts2 - Reverse string in two
+ * puts2 - Print every other character of a string
  *
- * This function takes in a string `str` and prints every
- * other character, starting with the first character.
+ * This function takes a string `str` and prints every other
+ * character, starting with the first character, followed by a new line.
  *
  * @str: Pointer to the string to be processed
  *
  * Return: None
  */
+
 void puts2(char *str)
 {
-    while (*str != '\0')
-    {
-        printf("%c", *str);
-        str += 2;
-    }
-    printf("\n");
+	char *ptr = str;
+	while (*ptr != '\0')
+	{
+		printf("%c", *ptr);
+		ptr += 2;
+	}
+	printf("\n");
 }
