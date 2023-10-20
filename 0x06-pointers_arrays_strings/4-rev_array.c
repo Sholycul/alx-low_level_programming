de45d4c0c2c464c0c91f395e8a945f2ed55f4516
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-20 14:25:41 -0700

    String in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - The function that reverses the content of an array of
+ * integers.
+ *
+ * @a: This is the input array
+ * @n: This is the positions have the array
+ *
+ * Return : none
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i, last;
+
+	last = n - 1;
+	for (i = 0; i < n / 2; i++)
+	{
+		int start, end;
+
+		start = a[i];
+		end = a[last];
+		a[i] = end;
+		a[last] = start;
+		last--;
+	}
+}
