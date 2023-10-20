3c4e8c1bf8c80bca6e14864191fa0f66e5f76547
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-20 14:10:28 -0700

    Comparing two strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
 /* Content of main.h goes here */
 
 #endif /* MAIN_H */
