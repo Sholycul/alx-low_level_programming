188086e1e500fc6774216f87377c56af5d6b4106
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-20 15:05:34 -0700

    Change case

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,8 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 
 void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
 /* Content of main.h goes here */
 
 #endif /* MAIN_H */
