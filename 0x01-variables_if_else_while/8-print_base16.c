ede409ad1af29617a2252917ff347565b48c7b74
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-20 17:33:45 -0700

    hexadecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - Print Hexadecimals
+*
+* Return: 0 for success
+*
+**/
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
