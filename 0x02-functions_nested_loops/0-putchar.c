0cf2e1bb103c20a408f8efb42148aa8c519a01d6
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-23 07:37:44 -0700

    Output with prinf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Write a program that prints Holberton, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
