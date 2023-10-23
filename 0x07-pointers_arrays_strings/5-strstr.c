9c575e418dba94f11942906f677f81c0f40fda8d
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-23 12:21:11 -0700

    still committing

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,31 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * _strstr - Write a function that locates a substring.
- *
- * @haystack: This is the input string
- * @needle: This is a find to locate in the string
- *
- * Return: a pointer to the beginning of the located substring, or NULL if the
- * substring is not found.
- */
-
+ * _strstr - finds the first occurrence of the substring.
+ * needle in the string haystack.
+ * @haystack: entire string.
+ * @needle: substring.
+ * Return: pointer to the beginning of located substring or
+ * NULL if the substring is not found.
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
-	{
-		char *temp;
+	char *bhaystack;
+	char *pneedle;
 
-		temp = haystack;
-		while (*needle == *haystack)
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
-			haystack++, needle++;
+			haystack++;
+			pneedle++;
 		}
-		if (*needle == '\0')
-			return (temp);
-		haystack++;
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
 	}
 	return (0);
 }
