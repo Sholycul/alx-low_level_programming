79dc2732fed4f94fbfb84df49ded6e20523fb33b
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-24 11:23:59 -0700

    Yeah

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,35 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_number - function to print integer unconventionally
- *
- * @n: integer passed
- *
- * Return: None
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
  */
-
 void print_number(int n)
 {
-	int divisor;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n = -n;
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
 	}
 
-	divisor = 1;
-	while (n / divisor >= 10)
-	{
-		divisor *= 10;
+	d = m;
+	count = 1;
 
-	}
-	while (divisor > 0)
+	while (d > 9)
 	{
-		_putchar((n / divisor) + '0');
-		n %= divisor;
-		divisor /= 10;
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
 	}
 }
