f254c8fdd07d502876d35115a39495db7cc36421
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-24 11:35:42 -0700

    prints a buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -24,6 +24,8 @@ void print_number(int n);
 int _putchar(char c);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
 /* Content of main.h goes here */
 
 #endif /* MAIN_H */
