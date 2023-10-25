99cd6f375eeb5001a6643dcf6de8bbb3f370b99c
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-25 12:39:40 -0700

    return string length

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	return(strlen(s));
+}
