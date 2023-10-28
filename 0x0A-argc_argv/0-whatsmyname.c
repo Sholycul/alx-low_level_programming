f1e50eed4f7091adefcb4cf8a80ef751b7622166
Sholycul <michaeloluwasholaiji2020@gmail.com>
2023-10-28 11:34:16 -0700

    What is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - it prints the function name
+ *
+ * @argc: integer number of arguments
+ * @argv: Characters themselves
+ *
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char* argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
